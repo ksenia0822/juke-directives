25ce1177750fe01c262fd91fbcd1668b7912c445
Ksenia Mikhailovskaya <ksenia0822@gmail.com>
2016-02-16 17:32:28 -0500

    mailto: half done

--- browser/js/module.js
+++ browser/js/module.js
@@ -1,8 +1,8 @@
 'use strict';
 
-var juke = angular.module('juke', ['ui.router', 'ngMessages']);
-
-juke.config(function($urlRouterProvider) {
-	$urlRouterProvider.when('', '/albums')
+var juke = angular.module('juke', ['ui.router', 'ngMessages'])
+.config(function($locationProvider, $urlRouterProvider) {
+	$locationProvider.html5Mode(true);
+	$urlRouterProvider.when('/', '/albums')
 	$urlRouterProvider.when('/artist/:artistId', '/artist/:artistId/albums')
 });
